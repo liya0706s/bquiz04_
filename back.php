72336b4db129d0e4d2728ee170fb5b0cd9bd54aa
Angie <liya0706s@gmail.com>
2024-03-21 21:25:55 +0800

    feat12:製作後台管理員權限的顯示不同選單功能

--- back.php
+++ back.php
@@ -21,14 +21,23 @@
 			</a>
 			<img src="./icon/0417.jpg">
 		</div>
+		<!-- 登入時有不同的管理者帳號權限限制 -->
+		<?php
+		$admin = $Admin->find(['acc' => $_SESSION['admin']]);
+		// unserialize()是把序列轉化成陣列
+		$pr = unserialize($admin['pr']);
+
+		?>
 		<div id="left" class="ct">
 			<div style="min-height:400px;">
 				<a href="?do=admin">管理權限設置</a>
-				<a href="?do=th">商品分類與管理</a>
-				<a href="?do=order">訂單管理</a>
-				<a href="?do=user">會員管理</a>
-				<a href="?do=bot">頁尾版權管理</a>
-				<a href="?do=news">最新消息管理</a>
+				<?php
+				echo in_array(1, $pr) ? "<a href='?do=th'>商品分類與管理</a>" : "";
+				echo in_array(2, $pr) ? "<a href='?do=order'>訂單管理</a>" : "";
+				echo in_array(3, $pr) ? "<a href='?do=user'>會員管理</a>" : "";
+				echo in_array(4, $pr) ? "<a href='?do=bot'>頁尾版權管理</a>" : "";
+				echo in_array(5, $pr) ? "<a href='?do=news'>最新消息管理</a>" : "";
+				?>
 				<a href="./api/logout.php?do=admin" style="color:#f00;">登出</a>
 			</div>
 		</div>
